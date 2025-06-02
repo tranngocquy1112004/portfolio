0b368c4b83e0457993cac73ba461393828af649b
tranngocquy1112004 <tranngocquy1818@gmail.com>
2025-06-02 23:32:56 +0700

    Portfolio

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import './Intro.css';
 import bg from '../../assets/bg.png'
 import cv from '../../assets/Tran-Ngoc-Quy-CV.pdf'
-import { Link } from "react-scroll";
+
 const Intro = () => {
     return (
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Tran Ngoc Quy</span> <br /> Website Designer</span>
-                <p className="introPara">I'm  a skilled Web Designer with experience in creating <br /> visual appealing and user-friendly websites</p>
-                <button className="btn"><a href={cv} download="Tran-Ngoc-Quy-CV.pdf">Download CV</a></button>
+                <p className="introPara">I'm a skilled Web Designer with experience in creating <br /> visual appealing and user-friendly websites</p>
+                <a href={cv} download="Tran-Ngoc-Quy-CV.pdf" className="btn">Download CV</a>
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -7,20 +7,20 @@ const Skills = () => {
     return (
         <section id="skills">
             <span className="skillTilte">What I do</span>
-            <span className="skillDesc">I'm  a skilled Web Designer with experience in creating visual appealing and user-friendly websites bádkjashdashdhaoohoqwnnqwqweoqwoenqwoneoqneoqwnoenoqwneonqwoenoqwneoqwnoenqownewsdasdasdaksnrqqwlnelkwqnlekqwnonvlkxlknlkxncwimoiwmeofoiefnwenweorcxsdasdasdawwqdwghgkyuiyuiytnbeec</span>
+            <span className="skillDesc">I am a passionate and motivated intern eager to gain hands-on experience in UI/UX and web design. I am enthusiastic about learning new design tools, understanding user needs, and creating visually appealing as well as user-friendly interfaces. My goal is to contribute to real projects, improve my skills, and grow as a designer in a professional environment.</span>
             <div className="skillBars">
                 <div className="skillBar">
                     <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
                     <div className="skillBarText">
                         <h2>UI/UX design</h2>
-                        <p>This is a demo text, you con write your own text in here</p>
+                        {/* <p>This is a demo text, you con write your own text in here</p> */}
                     </div>
                 </div>
                 <div className="skillBar">
                     <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
                     <div className="skillBarText">
                         <h2>Web Design</h2>
-                        <p>This is a demo text, you con write your own text in here</p>
+                        {/* <p>This is a demo text, you con write your own text in here</p> */}
                     </div>
                 </div>
             </div>
